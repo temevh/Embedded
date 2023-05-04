532a95939bb9efe3cbca63b3a55695d0092458ee
Matias Tarvainen <tarvainen.matias@gmail.com>
2023-05-04 19:43:11 +0300

    added lcd example
    also changed the uno master a little

---
+++ Arduino_lcd_c/Arduino_lcd_c/main.c
@@ -0,0 +1,29 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "lcd.h"
+#include <stdlib.h>
+
+int main(void)
+{
+	lcd_init(LCD_DISP_ON);
+	
+	int16_t test_number = 1023;
+	char test_char_array[16];
+	itoa(test_number, test_char_array, 10);
+	
+	lcd_clrscr();
+	
+	lcd_puts("Hello World!");
+	
+	lcd_gotoxy(0,1);
+	lcd_puts(test_char_array);
+	
+    while (1) 
+    {
+		
+    }
+}
+
+7
--- unoMaster/unoMaster/main.c
+++ unoMaster/unoMaster/main.c
@@ -1,11 +1,4 @@
-/*
- * mega_SPI_master_isr.c
- *
- * Created: 4.7.2019 8:53:41
- * Author : Glutex
- */ 
-
-/* ATmega2560 (MEGA2560) is master */
+/* uno is master */
 
 #define F_CPU 16000000UL
 #define FOSC 16000000UL // Clock Speed
@@ -17,9 +10,7 @@
 #include <util/setbaud.h>
 #include <stdio.h>
 
-static void
-USART_init(uint16_t ubrr)
-{
+static void USART_init(uint16_t ubrr){
     /* Set baud rate in the USART Baud Rate Registers (UBRR) */
     UBRR0H = (unsigned char) (ubrr >> 8);
     UBRR0L = (unsigned char) ubrr;
@@ -29,30 +20,19 @@ USART_init(uint16_t ubrr)
     
     /* Set frame format: 8 bit data, 2 stop bit */
     UCSR0C |= (1 << USBS0) | (3 << UCSZ00);
-    
 }
 
-static void
-USART_Transmit(unsigned char data, FILE *stream)
-{
+static void USART_Transmit(unsigned char data, FILE *stream){
     /* Wait until the transmit buffer is empty*/
-    while(!(UCSR0A & (1 << UDRE0)))
-    {
-        ;
-    }
+    while(!(UCSR0A & (1 << UDRE0))){;}
     
     /* Puts the data into a buffer, then sends/transmits the data */
     UDR0 = data;
 }
 
-static char
-USART_Receive(FILE *stream)
-{
+static char USART_Receive(FILE *stream){
     /* Wait until the transmit buffer is empty*/
-    while(!(UCSR0A & (1 << UDRE0)))
-    {
-        ;
-    }
+    while(!(UCSR0A & (1 << UDRE0))){;}
     
     /* Get the received data from the buffer */
     return UDR0;
@@ -62,9 +42,7 @@ USART_Receive(FILE *stream)
 FILE uart_output = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE);
 FILE uart_input = FDEV_SETUP_STREAM(NULL, USART_Receive, _FDEV_SETUP_READ);
 
-int 
-main(void)
-{
+int main(void){
     /* set SS, MOSI and SCK as output, pins 16 (PB2), 17 (PB3) and 19 (PB5) */
     DDRB |= (1 << PB2) | (1 << PB3) | (1 << PB5); // SS as output
     /* set SPI enable and master/slave select, making MEGA the master */
@@ -81,34 +59,45 @@ main(void)
     
     unsigned char spi_send_data[20] = "IDLE\n";
     unsigned char spi_receive_data[20];
+	
+	
+	
+	lcd_init(LCD_DISP_ON);
+	
+	int16_t test_number = 1023;
+	char test_char_array[16];
+	itoa(test_number, test_char_array, 10);
+	
+	lcd_clrscr();
+	
+	lcd_puts("Hello World!");
+	
+	lcd_gotoxy(0,1);
+	lcd_puts(test_char_array);
+	
+	
+	
+	
+	
+	
     
     /* send message to slave and receive message from slave */
-    while (1) 
-    {
+    while (1) {
         /* send byte to slave and receive a byte from slave */
         PORTB &= ~(1 << PB2); // SS LOW
             
-        for(int8_t spi_data_index = 0; spi_data_index < sizeof(spi_send_data); spi_data_index++)
-        {
+        for(int8_t spi_data_index = 0; spi_data_index < sizeof(spi_send_data); spi_data_index++){
                 
             SPDR = spi_send_data[spi_data_index]; // send byte using SPI data register
             _delay_us(5);
-            while(!(SPSR & (1 << SPIF)))
-            {
-                /* wait until the transmission is complete */
-                ;
-            }
+            while(!(SPSR & (1 << SPIF))){/* wait until the transmission is complete */;}
             _delay_us(5);
-            spi_receive_data[spi_data_index - 1] = SPDR; // receive byte from the SPI data register
-            
+            spi_receive_data[spi_data_index - 1] = SPDR; // receive byte from the SPI data register     
         }
             
         PORTB |= (1 << PB2); // SS HIGH
-        
         printf(spi_receive_data);
         _delay_ms(500);
-
-    }
-    
+    } 
     return 0;
 }
